f05b3c0911ecd8f78ff594ca5cbd15a96a59fae6
DESKTOP-QKJ565Q\Nestor Puentes <nestorpuentesin@gmail.com>
2020-10-22 17:10:14 -0500

    Se agrego un Activity de portada con diseño. Tambien se agregaron 3 botones en el diseño de la calculadora, 'Boton para borrar solo lo que esta en el display', 'Boton para enviar (provando el envio de datos - Intent explicito)' y 'Boton borrar (borrara solo un digito a la vez'.

---
+++ app/src/main/java/com/nestor/primerapp/Portada.java
@@ -0,0 +1,30 @@
+package com.nestor.primerapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+
+public class Portada extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_portada);
+
+        new CountDownTimer(4000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+            }
+
+            @Override
+            public void onFinish() {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+                finish();
+            }
+        }.start();
+    }
+
+}
